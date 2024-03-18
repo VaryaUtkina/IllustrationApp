f14360605770f3e37d648f770e07d13b7c61b4e9
Varya Utkina <varya2412@mail.ru>
2024-03-18 20:33:56 +0300

    Initial Commit

---
+++ IllustrationApp/IllustrationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IllustrationAppApp.swift
+//  IllustrationApp
+//
+//  Created by Варвара Уткина on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct IllustrationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
