df2eb8540241506c39e59571a7391fef96c02fd3
Varya Utkina <varya2412@mail.ru>
2024-03-19 22:46:19 +0300

    Add animation to text on HomeView

--- IllustrationApp/IllustrasionAppViews/HomeView.swift
+++ IllustrationApp/IllustrasionAppViews/HomeView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var animationStart = false
+    @State private var pawAnimationStart = false
     @State private var isPresented = false
+    @State private var textOpacity = 0.7
     
     var body: some View {
         ZStack {
@@ -24,72 +26,72 @@ struct HomeView: View {
                     .offset(
                         x: width - 19 * width / 20,
                         y: height - 2 * height / 20
-                )
-                    .opacity(animationStart ? 0.8 : 0)
+                    )
+                    .opacity(pawAnimationStart ? 0.8 : 0)
                     .animation(
                         .easeInOut(duration: 0.5),
-                        value: animationStart
+                        value: pawAnimationStart
                     )
                 
                 PawView()
                     .offset(
                         x: width - 13 * width / 20,
                         y: height - 4 * height / 20
-                )
-                    .opacity(animationStart ? 0.8 : 0)
+                    )
+                    .opacity(pawAnimationStart ? 0.8 : 0)
                     .animation(
                         .easeInOut(duration: 0.5).delay(0.1),
-                        value: animationStart
+                        value: pawAnimationStart
                     )
                 PawView()
                     .offset(
                         x: width - 11 * width / 20,
                         y: height - 9 * height / 20
-                )
-                    .opacity(animationStart ? 0.8 : 0)
+                    )
+                    .opacity(pawAnimationStart ? 0.8 : 0)
                     .animation(
                         .easeInOut(duration: 0.5).delay(0.2),
-                        value: animationStart
+                        value: pawAnimationStart
                     )
                 PawView()
                     .offset(
                         x: width - 16 * width / 20,
                         y: height - 12 * height / 20
-                )
-                    .opacity(animationStart ? 0.8 : 0)
+                    )
+                    .opacity(pawAnimationStart ? 0.8 : 0)
                     .animation(
                         .easeInOut(duration: 0.5).delay(0.3),
-                        value: animationStart
+                        value: pawAnimationStart
                     )
                 PawView()
                     .offset(
                         x: width - 9 * width / 20,
                         y: height - 14 * height / 20
-                )
-                    .opacity(animationStart ? 0.8 : 0)
+                    )
+                    .opacity(pawAnimationStart ? 0.8 : 0)
                     .animation(
                         .easeInOut(duration: 0.5).delay(0.4),
-                        value: animationStart
+                        value: pawAnimationStart
                     )
                 PawView()
                     .offset(
                         x: width - 13 * width / 20,
                         y: height - 17 * height / 20
-                )
-                    .opacity(animationStart ? 0.8 : 0)
+                    )
+                    .opacity(pawAnimationStart ? 0.8 : 0)
                     .animation(
                         .easeInOut(duration: 0.5).delay(0.5),
-                        value: animationStart
+                        value: pawAnimationStart
                     )
                 PawView()
                     .offset(
                         x: width - 7 * width / 20,
                         y: height - 19 * height / 20
-                )
-                    .opacity(animationStart ? 0.8 : 0)
+                    )
+                    .opacity(pawAnimationStart ? 0.8 : 0)
                     .animation(
                         .easeInOut(duration: 0.5).delay(0.6),
-                        value: animationStart
+                        value: pawAnimationStart
                     )
                 VStack {
                     Spacer()
@@ -104,6 +106,15 @@ struct HomeView: View {
                         Spacer()
                     }
                     Spacer()
+                    Text("Tap on screen to \(pawAnimationStart ? "hide" : "show") paws")
+                        .font(.headline)
+                        .foregroundStyle(Color.start)
+                        .opacity(textOpacity)
+                        .animation(
+                            .easeInOut(duration: 0.5),
+                            value: textOpacity
+                        )
+                    
                 }
             }
         }
@@ -112,6 +123,18 @@ struct HomeView: View {
         }
         .onAppear() {
             animationStart.toggle()
+            pawAnimationStart.toggle()
+        }
+        .onTapGesture {
+            withAnimation(.easeOut(duration: 0.25)) {
+                textOpacity = 0
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                pawAnimationStart.toggle()
+                withAnimation(.easeIn(duration: 0.75)) {
+                    textOpacity = 0.7
+                }
+            }
         }
     }
 }
